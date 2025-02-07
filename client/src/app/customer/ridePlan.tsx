7a691117707c925909a403cc91e8a66340c85948
zohaib <muhammadzohaibmalik10@gmail.com>
2025-02-07 15:52:21 +0500

    Add Google Places Autocomplete and update ride plan layout

--- client/src/app/customer/ridePlan.tsx
+++ client/src/app/customer/ridePlan.tsx
@@ -1,19 +1,38 @@
 import { customMapStyle, islamabadInitialRegion } from "@/utils/CustomMap";
 import { useState } from "react";
-import { KeyboardAvoidingView, View, StyleSheet } from "react-native";
+import { KeyboardAvoidingView, View, StyleSheet, TouchableOpacity, Text } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import MapViewDirections from "react-native-maps-directions";
+import { AntDesign } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import 'react-native-get-random-values'; // Fix for crypto.getRandomValues()
+
+const GOOGLE_MAPS_API_KEY = process.env.EXPO_PUBLIC_MAP_API_KEY;
+console.log(GOOGLE_MAPS_API_KEY)
+
+
+import { router } from "expo-router";
+import { windowHeight } from "@/utils/Constants";
+import { mapStyles } from "@/styles/mapStyles";
+
 
 const RidePlan = () => {
   const [region, setRegion] = useState(
-   islamabadInitialRegion
+    islamabadInitialRegion
   );
 
   const [marker, setMarker] = useState(null);
   const [currentLocation, setCurrentLocation] = useState(null);
+  const [keyboardAvoidingHeight, setkeyboardAvoidingHeight] = useState(false);
+  const [places, setPlaces] = useState<any>([]);
+  const [query, setQuery] = useState("");
+  const handleInputChange = (text: any) => {
+    setQuery(text);
+  }
 
   return (
-    <KeyboardAvoidingView style={styles.container}>
+    <KeyboardAvoidingView style={mapStyles.container}>
       {/* Map Section (70% Height) */}
       <View style={styles.mapContainer}>
         <MapView
@@ -37,8 +56,77 @@ const RidePlan = () => {
       </View>
 
       {/* Bottom Section (30% Height) */}
-      <View style={styles.bottomSection}>
-        {/* Add additional UI elements here */}
+      <View className="flex-1 ">
+        <View className="flex-1">
+          <View className="flex flex-row items-center justify-between">
+            <TouchableOpacity onPress={() => router.back()}>
+              <AntDesign name="arrowleft" size={24} color="black" />
+            </TouchableOpacity>
+            <Text className="m-auto text-lg font-JakartaMedium">Ride Plan</Text>
+          </View>
+        </View>
+
+        <View style={{
+          borderColor: "#000",
+          borderWidth: 2,
+          borderRadius: 15,
+          marginBottom: windowHeight(10),
+          paddingHorizontal: windowHeight(10),
+          paddingVertical: windowHeight(10),
+        }}>
+          <View className="flex flex-row items-center m-1">
+            <FontAwesome name="map-marker" size={32} color="blue" />
+            <View style={{ flex: 1, marginLeft: 8 }}>
+              <Text className="font-JakartaMedium text-[#075BB5]">Current Location</Text>
+              <GooglePlacesAutocomplete
+                placeholder="Where to?"
+                onPress={(data, details = null) => {
+                  setkeyboardAvoidingHeight(true);
+                  setPlaces([
+                    {
+                      description: data.description,
+                      details: details,
+                    }
+                  ]);
+                }}
+                query={{
+                  key: `${process.env.EXPO_PUBLIC_MAP_API_KEY}`,
+                  language: 'en',
+                }}
+                styles={{
+                  textInputContainer: {
+                    width: '100%',
+                  },
+                  textInput: {
+                    height: 38,
+                    fontSize: 16,
+                    color: '#000',
+                    borderBottomWidth: 1,
+                    borderColor: "#ccc",
+                    paddingHorizontal: 10,
+                    borderRadius: 10,
+                    backgroundColor: "#fff",
+                    textAlignVertical: "center",
+                  },
+                  predefinedPlacesDescription: {
+                    // color: '#1faadb'
+                  }
+                }}
+                textInputProps={{
+                  onChangeText: (text) => handleInputChange(text),
+                  value: query,
+                  onFocus: () => setkeyboardAvoidingHeight(true),
+                }}
+                onFail={(error) => console.log(error)}
+                fetchDetails={true}
+                debounce={200}
+              />
+            </View>
+          </View>
+
+
+        </View>
+
       </View>
     </KeyboardAvoidingView>
   );
@@ -55,11 +143,7 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
   },
-  bottomSection: {
-    height: "30%",
-    backgroundColor: "white",
-    padding: 16,
-  },
+
 });
 
 export default RidePlan;
--- client/src/utils/Constants.tsx
+++ client/src/utils/Constants.tsx
@@ -1,11 +1,26 @@
-import { Dimensions } from "react-native";
+import { Dimensions, DimensionValue, PixelRatio } from "react-native";
 
-export const screenHeight = Dimensions.get('screen').height
-export const screenWidth = Dimensions.get('screen').width
+export const screenHeight = Dimensions.get('window').height
+export const screenWidth = Dimensions.get('window').width
 
+export const windowHeight = (height: DimensionValue): number => {
+  if (!height) {
+    return 0;
+  }
+  let tempHeight = screenHeight * (parseFloat(height.toString()) / 667);
+  return PixelRatio.roundToNearestPixel(tempHeight);
+};
+
+export const windowWidth = (width: DimensionValue): number => {
+  if (!width) {
+    return 0;
+  }
+  let tempWidth = screenWidth * (parseFloat(width.toString()) / 480);
+  return PixelRatio.roundToNearestPixel(tempWidth);
+};
 
 export enum Colors {
-    primary = '#FFCA1F',
+    primary = '#FFCA1F',    
     background = '#fff',
     text = '#222',
     theme = '#CF551F',
