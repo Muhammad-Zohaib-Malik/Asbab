d0a226bb25fde991fda716eb7d420101d2c631bd
zohaib <muhammadzohaibmalik10@gmail.com>
2025-02-14 22:18:04 +0500

    Add update profile route with JWT verification

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { refreshToken, auth } = require("../controllers/auth");
+const { refreshToken, auth, updateProfileController } = require("../controllers/auth");
+const verifyJWT = require("../middleware/authentication");
 
 router.post("/refresh-token", refreshToken);
 router.post("/signin", auth);
-
+router.put("/update-profile", verifyJWT, updateProfileController)
 module.exports = router;
