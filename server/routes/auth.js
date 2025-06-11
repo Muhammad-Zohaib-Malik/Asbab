092bebee6aa4a1dc6eb403305a74a910e7dcf9f7
zohaib <muhammadzohaibmalik10@gmail.com>
2025-06-11 11:30:19 +0500

    fix typo

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { refreshToken, auth, updateProfileController, updateProfilePicContoller } = require("../controllers/auth");
+const { refreshToken, auth, updateProfileController, updateProfilePicController } = require("../controllers/auth");
 const verifyJWT = require("../middleware/authentication");
 const { upload } = require("../middleware/multer");
 
 router.post("/refresh-token", refreshToken);
 router.post("/signin", auth);
 router.put("/update-profile", verifyJWT, updateProfileController)
-router.put("/update-profile-pic", verifyJWT, upload.single('profilePic'), updateProfilePicContoller)
+router.put("/update-profile-pic", verifyJWT, upload.single('profilePic'), updateProfilePicController)
 
 
 module.exports = router;
