c384c284c5870428b0c671dc0b7497597744049b
zohaib <muhammadzohaibmalik10@gmail.com>
2025-07-01 10:50:06 +0500

    refactor: improve code formatting and structure in RootLayout component

--- client/src/app/_layout.tsx
+++ client/src/app/_layout.tsx
@@ -32,24 +32,69 @@ export default function RootLayout() {
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <StripeProvider publishableKey={process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY}>
+      <StripeProvider
+        publishableKey={process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY}
+      >
         <WSProvider>
           <Stack>
             <Stack.Screen name="index" options={{ headerShown: false }} />
             <Stack.Screen name="role" options={{ headerShown: false }} />
-            <Stack.Screen name="customer/auth" options={{ headerShown: false }} />
-            <Stack.Screen name="captain/auth" options={{ headerShown: false }} />
-            <Stack.Screen name="captain/home" options={{ headerShown: false }} />
-            <Stack.Screen name="captain/liveride" options={{ headerShown: false }} />
-            <Stack.Screen name="customer/home" options={{ headerShown: false }} />
-            <Stack.Screen name="customer/selectlocations" options={{ headerShown: false }} />
-            <Stack.Screen name="customer/ridebooking" options={{ headerShown: false }} />
-            <Stack.Screen name="customer/profile" options={{ headerShown: false }} />
-            <Stack.Screen name="customer/setting" options={{ headerShown: false }} />
-            <Stack.Screen name="customer/complaints" options={{ headerShown: false }} />
-            <Stack.Screen name="customer/liveride" options={{ headerShown: false }} />
-            <Stack.Screen name="customer/myrides" options={{ headerShown: false }} />
-            <Stack.Screen name="customer/payment" options={{ headerShown: false }} />
+            <Stack.Screen
+              name="customer/auth"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="captain/auth"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="captain/home"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="captain/liveride"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="captain/setting"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="customer/home"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="customer/selectlocations"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="customer/ridebooking"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="customer/profile"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="customer/setting"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="customer/complaints"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="customer/liveride"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="customer/myrides"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="customer/payment"
+              options={{ headerShown: false }}
+            />
             <Stack.Screen name="+not-found" />
           </Stack>
         </WSProvider>
