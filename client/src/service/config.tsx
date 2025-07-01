c59b12cdd9d1a128e13240ad81f004c6d44f23e2
zohaib <muhammadzohaibmalik10@gmail.com>
2025-07-01 10:50:13 +0500

    fix: update BASE_URL and SOCKET_URL to new server address

--- client/src/service/config.tsx
+++ client/src/service/config.tsx
@@ -1,8 +1,8 @@
 import { Platform } from "react-native";
 
-export const BASE_URL = "http://192.168.0.102:3000";
+export const BASE_URL = "http://192.168.170.209:3000";
 
 export const SOCKET_URL =
   Platform.OS === "ios"
-    ? "ws://192.168.0.102:3000"
-    : "http://192.168.0.102:3000";
+    ? "ws://192.168.170.209:3000"
+    : "http://192.168.170.209:3000";
