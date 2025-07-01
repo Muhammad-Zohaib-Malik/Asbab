201686a0ea83f0ea81b9d1c88fa6b8b547a0ad3b
zohaib <muhammadzohaibmalik10@gmail.com>
2025-07-01 10:49:56 +0500

    refactor: improve code formatting and structure in Role component

--- client/src/app/role.tsx
+++ client/src/app/role.tsx
@@ -1,45 +1,49 @@
-import { roleStyles } from '@/styles/roleStyles';
-import { router } from 'expo-router';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
-
+import { roleStyles } from "@/styles/roleStyles";
+import { router } from "expo-router";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 
 const Role = () => {
+  const handleCustomerPress = () => {
+    router.navigate("/customer/auth");
+  };
+  const handleCaptainPress = () => {
+    router.navigate("/captain/auth");
+  };
 
-    const handleCustomerPress = () => {
-        router.navigate('/customer/auth')
+  return (
+    <View style={roleStyles.container}>
+      <Image
+        source={require("@/assets/images/Logo.png")}
+        style={roleStyles.logo}
+      />
+      <Text className="font-JakartaMedium text-[20px] text-[#075BB5]">
+        Select Who you are
+      </Text>
 
-    }
-    const handleCaptainPress = () => {
-        router.navigate('/captain/auth')
-
-
-    }
-
-    return (
-        <View style={roleStyles.container}>
-            <Image source={require('@/assets/images/Logo.png')}
-                style={roleStyles.logo} />
-            <Text className='font-JakartaMedium text-[20px] text-[#075BB5]'>Select Who you are</Text>
-
-            <TouchableOpacity style={roleStyles.card} onPress={handleCustomerPress}>
-                <Image source={require('@/assets/images/customer.png')} style={roleStyles.image} />
-                <View style={roleStyles.cardContent}>
-                    <Text className='font-JakartaMedium text-[15px] text-[#075BB5]'>Customer</Text>
-                </View>
-
-            </TouchableOpacity>
-
-            <TouchableOpacity style={roleStyles.card} onPress={handleCaptainPress}>
-                <Image source={require('@/assets/images/captain.png')} style={roleStyles.image} />
-                <View style={roleStyles.cardContent}>
-                    <Text className='font-JakartaMedium text-[15px] text-[#075BB5]'>Captain</Text>
-                </View>
-
-            </TouchableOpacity>
-
+      <TouchableOpacity style={roleStyles.card} onPress={handleCustomerPress}>
+        <Image
+          source={require("@/assets/images/customer.png")}
+          style={roleStyles.image}
+        />
+        <View style={roleStyles.cardContent}>
+          <Text className="font-JakartaMedium text-[15px] text-[#075BB5]">
+            Customer
+          </Text>
         </View>
+      </TouchableOpacity>
 
-    )
-
-}
+      <TouchableOpacity style={roleStyles.card} onPress={handleCaptainPress}>
+        <Image
+          source={require("@/assets/images/captain.png")}
+          style={roleStyles.image}
+        />
+        <View style={roleStyles.cardContent}>
+          <Text className="font-JakartaMedium text-[15px] text-[#075BB5]">
+            Captain
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
 export default Role;
